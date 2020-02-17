00afb6d9a4c66cfd103e3e8b77f00e34acd80c85
RajibTheKing <aporba.das@gmail.com>
2020-02-18 02:36:03 +0600

    final report.html

--- Detect_Sensitive_Code.py
+++ Detect_Sensitive_Code.py
@@ -1,26 +1,31 @@
 import re
 import subprocess
-import Utility
-import ParenthesisBalance
 import statistics
 import copy
+import Utility
+import ParenthesisBalance
+import HighlightExecutedStatements
 
 class Detect_Sensitive_Code:
+    
     utility = None
     parenthesisBalance = None
+    highlightStatements = None 
 
     def __init__(self, inputFileName):
         
         self.utility = Utility.Utility()
         self.parenthesisBalance = ParenthesisBalance.Parenthesis_Balance()
+        self.highlightStatements = HighlightExecutedStatements.Highlight_Executed_Statements()
+
         processedFileName = self.removeEmptyLines(inputFileName) # -> InputA.cpp
         allFunctionName = self.getAllFunctionName(processedFileName) #-> allFunction
         processedFileName = self.addLineNumberBeforeStatement(processedFileName) # -> InputB.cpp
         getTrackOfBlock = self.parenthesisBalance.TrackCalculation(processedFileName) #Function, Loop, If track
         processedFileName = self.addBlockwiseLineNumber(processedFileName, getTrackOfBlock) #-> inputC.cpp
         processedFileName = self.executeProcessedSourceCode(processedFileName)
-        self.highlightStatements(processedFileName)
-        self.highlightHeuristics(processedFileName , getTrackOfBlock, allFunctionName)
+        self.highlightStatements.highlightExecutedStatements(processedFileName)
+        #self.highlightHeuristics(processedFileName , getTrackOfBlock, allFunctionName)
         print("Operation Successful")
         
 
@@ -269,123 +274,9 @@ class Detect_Sensitive_Code:
         return outputFileName 
 
 
-    def getColorByRank(self, rank):
-        if rank == 4:
-            return "red"
-        if rank == 3:
-            return "orange"
-        if rank == 2:
-            return "blue"
-        if rank == 1:
-            return "#00d600"
-        return "black"
-        
-
-    def highlightStatements(self, inputFileName):
-
-        f2 = open('Output.txt')
-        l2 = f2.read().splitlines()
-
-        lineNumbers = []
-        for line in l2:
-            word = line.split()
-            if word[0]=='line':
-                lineNumbers.append(int(word[2]))
-
-        rank = dict()
-        countTrac = dict()
-        tempList = copy.deepcopy(lineNumbers)
-
-        for x in tempList:
-            if x not in countTrac.keys():
-                countTrac[x] = 1
-            else:
-                countTrac[x] += 1
-        
-        #print(countTrac)
-
-        sorted_x = sorted(countTrac.items(), key=lambda kv: kv[1])
-        sorted_x.reverse()
-        #print(sorted_x)
-        max_freq = sorted_x[0][1]
-        min_freq = sorted_x[len(sorted_x)-1][1]
 
 
-
-
-        resRank = 0
-        for (lineNumber, freq) in sorted_x:
-            if freq >= max_freq:
-                resRank = 4
-            elif freq == min_freq :
-                resRank = 1
-            elif freq == 2:
-                resRank = 2
-            elif freq<max_freq and freq>2:
-                resRank = 3
-            else:
-                resRank = 0
-            rank[lineNumber] = self.getColorByRank(resRank)
-                
-            
-
-        #print(lineNumbers)
-
-        f3 = open('inputA.cpp')
-        l3 = f3.read().splitlines()
-
-        counter = 0
-        stringfff = "<html><body><code>\n"
-        for line in l3:
-            counter = counter + 1
-            if(line.startswith("#")):
-                line = self.utility.Handeling_HeaderFile(line)
-            
-            if "global_loop_id" in line and "global_ifelse_id" in line and "global_function_id" in line:
-                stringfff +=''
-
-            if('freopen("Output.txt", "w+", stdout);' in line):
-                stringfff += ''
-                
-            elif counter in lineNumbers:
-                cc = 0
-                for x in lineNumbers:
-                    if x == counter:
-                        cc+=1
-            
-                if counter in rank.keys():
-                    curColor = rank[counter]
-                else:
-                    curColor = "black"
-                
-                if curColor =="red":    
-                    stringfff += '<p style="font-family:verdana;font-weight:bold;color:'+curColor+'">' + line + '   //(MOST SENSITIVE portion), executed ' + str(cc) + ' times' + '</p>\n'
-                elif cc == 1:
-                    stringfff += '<p style="font-family:verdana;font-weight:bold;color:'+curColor+'">' + line + '</p>\n'
-                else:
-                    stringfff += '<p style="font-family:verdana;font-weight:bold;color:'+curColor+'">' + line + '   //executed ' + str(cc) + ' times' + '</p>\n'    
-
-            else:
-                stringfff += '<p style="font-family:verdana;font-weight:bold;color:black">' + line + '</p>\n'
-            
-
-        stringfff += "</code></body></html>\n"
-
-        f2.close()
-        f3.close()
-        f = open("report.html", "w")
-        f.write(stringfff)
-        f.close()
-
-
-    def calculateOccurrences(self, linenumbers, st, ed):
-        cnt = 0
-        for i in range(0, len(linenumbers)):
-            if st <= linenumbers[i] and linenumbers[i] <= ed:
-                cnt+=1
-        return cnt
-
-    def calculateStatementsForLoopHeuristics(self, instruction_set):
+    def calculateStatementsForHeuristics(self, instruction_set):
         instruction_set.append([-1, -1])
         ret = {}
         st = []
@@ -404,27 +295,6 @@ class Detect_Sensitive_Code:
             else:
                 st.append(ls)
         
-        return ret
-    
-    def calculateStatementsForIfElseHeuristics(self, instruction_set):
-        instruction_set.append([-1, -1])
-        ret = {}
-        st = []
-        st.append([-1,-1])
-        for ls in instruction_set:
-            sz = len(st)
-            if ls[0] == 0:
-                for ss in st:
-                    tup = (ss[0], ss[1])
-                    if tup in ret:
-                        ret[tup] += 1
-                    else:
-                        ret[tup] = 1
-            elif ls[0] == st[sz - 1][0]:
-                st.pop()
-            else:
-                st.append(ls)
-        #print(ret)
         return ret
 
 
@@ -476,6 +346,7 @@ class Detect_Sensitive_Code:
                     function_temp.append(temp)
                     continue
         
+        '''
         if_dict = self.parenthesisBalance.IfTrackCalculation(trackOfBlock)
         else_dict = self.parenthesisBalance.ElseTrackCalculation(trackOfBlock)
         elseIf_dict = self.parenthesisBalance.ElseIfTrackCalculation(trackOfBlock)
@@ -486,12 +357,12 @@ class Detect_Sensitive_Code:
         mergedIfElse.update(if_dict)
         mergedIfElse.update(else_dict)
         mergedIfElse.update(elseIf_dict)
-        
+        '''
         #print(loop_temp)
         
 
-        loop_final = self.calculateStatementsForLoopHeuristics(loop_temp)
-        ifelse_final = self.calculateStatementsForIfElseHeuristics(ifelse_temp)
+        loop_final = self.calculateStatementsForHeuristics(loop_temp)
+        ifelse_final = self.calculateStatementsForHeuristics(ifelse_temp)
         function_nested_cnt = self.functioncall_Check( fn_name , loop_temp)
         #function_final = self.calculateStatementsForHeuristics(function_temp)
 
---
+++ HighlightExecutedStatements.py
@@ -0,0 +1,117 @@
+import Utility
+import copy
+
+class Highlight_Executed_Statements:
+    utility = None
+
+    def __init__(self):
+
+        self.utility = Utility.Utility() 
+        print("Successful Highlighting Executed Statements")
+
+
+    def getColorByRank(self, rank):
+        if rank == 4:
+            return "red"
+        if rank == 3:
+            return "orange"
+        if rank == 2:
+            return "blue"
+        if rank == 1:
+            return "#00d600"
+        return "black"
+        
+
+    def highlightExecutedStatements(self, inputFileName):
+
+        f2 = open(inputFileName)
+        l2 = f2.read().splitlines()
+
+        lineNumbers = []
+        for line in l2:
+            word = line.split()
+            if word[0]=='line':
+                lineNumbers.append(int(word[2]))
+
+        rank = dict()
+        countTrac = dict()
+        tempList = copy.deepcopy(lineNumbers)
+
+        for x in tempList:
+            if x not in countTrac.keys():
+                countTrac[x] = 1
+            else:
+                countTrac[x] += 1
+        
+        #print(countTrac)
+
+        sorted_x = sorted(countTrac.items(), key=lambda kv: kv[1])
+        sorted_x.reverse()
+        #print(sorted_x)
+        max_freq = sorted_x[0][1]
+        min_freq = sorted_x[len(sorted_x)-1][1]
+
+
+
+
+        resRank = 0
+        for (lineNumber, freq) in sorted_x:
+            if freq >= max_freq:
+                resRank = 4
+            elif freq == min_freq :
+                resRank = 1
+            elif freq == 2:
+                resRank = 2
+            elif freq<max_freq and freq>2:
+                resRank = 3
+            else:
+                resRank = 0
+            rank[lineNumber] = self.getColorByRank(resRank)
+                
+
+        f3 = open('inputA.cpp')
+        l3 = f3.read().splitlines()
+
+        counter = 0
+        stringfff = "<html><body><code>\n"
+
+        for line in l3:
+            counter = counter + 1
+
+            if counter == 1:
+                continue
+
+            if(line.startswith("#")):
+                line = self.utility.Handeling_HeaderFile(line)
+
+            if('freopen("Output.txt", "w+", stdout);' in line):
+                stringfff += '<p style="font-family:verdana;font-weight:bold;color:black">' + '{' + '</p>\n'
+                
+            elif counter in lineNumbers:
+                cc = 0
+                for x in lineNumbers:
+                    if x == counter:
+                        cc+=1
+            
+                if counter in rank.keys():
+                    curColor = rank[counter]
+                else:
+                    curColor = "black"
+                
+                if curColor =="red":    
+                    stringfff += '<p style="font-family:verdana;font-weight:bold;color:'+curColor+'">' + line + '   //(MOST SENSITIVE portion), executed ' + str(cc) + ' times' + '</p>\n'
+                elif cc == 1:
+                    stringfff += '<p style="font-family:verdana;font-weight:bold;color:'+curColor+'">' + line + '</p>\n'
+                else:
+                    stringfff += '<p style="font-family:verdana;font-weight:bold;color:'+curColor+'">' + line + '   //executed ' + str(cc) + ' times' + '</p>\n'    
+
+            else:
+                stringfff += '<p style="font-family:verdana;font-weight:bold;color:black">' + line + '</p>\n'
+            
+        stringfff += "</code></body></html>\n"
+
+        f2.close()
+        f3.close()
+        f = open("report.html", "w")
+        f.write(stringfff)
+        f.close()
